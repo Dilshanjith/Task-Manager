d86a8cdab8d4b22a6a6f00dc998de837164f72c4
Dilshan samarasinghe <113366721+Dilshanjith@users.noreply.github.com>
2026-04-23 16:27:55 +0530

    feat: implement backend assembly configuration and frontend task API service with Firebase authentication

--- Backend/obj/Debug/net8.0/Backend.AssemblyInfo.cs
+++ Backend/obj/Debug/net8.0/Backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9a83b705eb69e2d17d6db52171df24ea232becb4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+df19855a449c1d9421cf792e7642583d7769a3e4")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
